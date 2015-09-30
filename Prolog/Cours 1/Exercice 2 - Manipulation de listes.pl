60438ac234a2671e57cf6f3ad19858bcbf2019ba
Razvan <stancioiu.razvan@gmail.com>
2015-09-30 23:25:33 +0200

    30/09/2015

--- Prolog/Cours 1/Exercice 2 - Manipulation de listes.pl
+++ Prolog/Cours 1/Exercice 2 - Manipulation de listes.pl
@@ -1,5 +1,3 @@
-
-
 % append an element to list
 append([ ], L1, L1).
 append([A|L1],L2,[A|L3]) :- append(L1,L2,L3).
@@ -14,5 +12,6 @@ inv([],[]).
 inv([X|L1],L2) :- inv(L1,Ret),append(Ret,[X],L2).
 
 % substitution of an element in a list
-%subsAll(_,_,[],L) :- !.
-%subsAll(p1,p2,[p1|L1],L2) :- !,subsAll(p1,p2,L1,Ret), append([p2],Ret,L2).
+subsAll(_,_,[],[]).
+subsAll(P1,P2,[P1|L1],[P2|L2]) :- subsAll(P1,P2,L1,L2).
+subsAll(P1,P2,[Y|L1],[Y|L2]) :- Y\=P1,subsAll(P1,P2,L1,L2).
